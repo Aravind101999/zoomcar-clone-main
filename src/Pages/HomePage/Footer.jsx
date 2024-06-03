e504091ac4f3cb838519dc9a99853d1ac1fb193d
Aravind101999 <63011485+Aravind101999@users.noreply.github.com>
2024-06-03 23:16:42 +0530

    first

--- src/Pages/HomePage/Footer.jsx
+++ src/Pages/HomePage/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box,  Flex, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import foot from "../../Styles/Footer.module.css";
 import {
